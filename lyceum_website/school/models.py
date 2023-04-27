92c861ced16e6cbf4f17cf44d82bb3a3ff7b050d
Антон <vriilux@yandex.ru>
2023-04-27 22:57:41 +0300

    fix

--- lyceum_website/school/models.py
+++ lyceum_website/school/models.py
@@ -3,8 +3,8 @@ from django.urls import reverse
 
 class School(models.Model):
     title = models.CharField(max_length=255, verbose_name='Заголовок')
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photo/%Y/%m/%d/")
+    content = models.TextField(blank=True, verbose_name='Содержимое статьи')
+    photo = models.ImageField(upload_to="photo/%Y/%m/%d/", verbose_name='Фото')
     time = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     is_publised = models.BooleanField(default=True, verbose_name='Опубликовано')
 
